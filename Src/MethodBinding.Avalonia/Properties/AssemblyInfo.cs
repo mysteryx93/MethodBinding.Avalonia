54808e77f0766bce88a2ef520e1c8ec98ef06285
Etienne Charland <mysteryx93@protonmail.com>
2021-12-16 11:07:05 -0500

    Update XAML namespace

--- Src/MethodBinding.Avalonia/Properties/AssemblyInfo.cs
+++ Src/MethodBinding.Avalonia/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using Avalonia.Metadata;
 
-[assembly: XmlnsDefinition("http://schemas.singulink.com/xaml", "Singulink.WPF.Data")]
+[assembly: XmlnsDefinition("https://github.com/mysteryx93/MethodBinding.Avalonia", "MethodBinding.Avalonia")]
